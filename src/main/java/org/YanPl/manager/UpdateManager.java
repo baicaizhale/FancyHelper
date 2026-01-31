70f66526aa8f3949d69922f95ec6c2b2f8b2714c
Yan Cai <2651557041@qq.com>
2026-01-31 18:16:53 +0800

    Update: 更新版本至2.3.0

--- src/main/java/org/YanPl/manager/UpdateManager.java
+++ src/main/java/org/YanPl/manager/UpdateManager.java
@@ -16,11 +16,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.plugin.java.JavaPlugin;
-
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.util.concurrent.TimeUnit;
